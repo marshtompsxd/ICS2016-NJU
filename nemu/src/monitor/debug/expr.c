47985b91fae33ca4e4d321897736dc134cf65583
tracer-ics2016 <tracer@njuics.org>
2016-08-31 21:15:10 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:39 UTC 2016 x86_64 GNU/Linux  21:15:10 up  8:20,  4 users,  load average: 0.06, 0.06, 0.07 ff0b335fd895e052fcaa72b14715deae5ed83d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,9 +5,10 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
 
 enum {
-	NOTYPE = 256, EQ
+	NOTYPE = 256,EQ
 
 	/* TODO: Add more token types */
 
@@ -23,9 +24,15 @@ static struct rule {
 	 */
 
 	{" +",	NOTYPE},				// spaces
+	{"==", EQ},						// equal
 	{"\\+", '+'},					// plus
-	{"==", EQ}						// equal
-};
+    {"-", '-'},                     // minus
+    {"\\*", '*'},                   // multiply
+    {"/", '/'},                     // divide
+    {"\\(", '('},                   // left bracket
+    {"\\)", ')'},                   // right bracket
+    {"[0-9]+", 'n'}                 // number
+}; 
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 
@@ -78,7 +85,29 @@ static bool make_token(char *e) {
 				 * of tokens, some extra actions should be performed.
 				 */
 
+
+                if(rules[i].token_type!=256){
+                    tokens[nr_token].type=rules[i].token_type;
+                    strncpy(tokens[nr_token].str,substr_start,substr_len);
+                    nr_token++;
+                }
+
+
 				switch(rules[i].token_type) {
+                    case '+': tokens[nr_token].type='+';tokens[nr_token].str[0]='+';nr_token++;break;
+                    case '-': tokens[nr_token].type='-';tokens[nr_token].str[0]='-';nr_token++;break;
+                    case '*': tokens[nr_token].type='*';tokens[nr_token].str[0]='*';nr_token++;break;
+                    case '/': tokens[nr_token].type='/';tokens[nr_token].str[0]='/';nr_token++;break;
+                    case '(': tokens[nr_token].type='(';tokens[nr_token].str[0]='(';nr_token++;break;
+                    case ')': tokens[nr_token].type=')';tokens[nr_token].str[0]=')';nr_token++;break;
+                    case 'n':{
+                        tokens[nr_token].type='n';
+                        strncpy(tokens[nr_token].str,substr_start,substr_len);
+                        nr_token++;
+                        break;
+                    }
+                    case NOTYPE:break;
+                    case EQ:break;
 					default: panic("please implement me");
 				}
 
@@ -95,14 +124,127 @@ static bool make_token(char *e) {
 	return true; 
 }
 
+
+bool check_parentheses(int p,int q){
+    int i;
+    if(tokens[p].type!='('||tokens[q].type!=')'){
+        return false;
+    }
+    int leftNum=0;
+    int rightNum=0;
+    for(i=p;i<=q;i++){
+
+        if(tokens[i].type=='(')leftNum++;
+        else if(tokens[i].type==')')rightNum++;
+
+        if(leftNum<rightNum)return false;
+        else if(leftNum==rightNum&&i<q)return false;
+    }
+    if(leftNum==rightNum)return true;
+    else return false;
+}
+
+
+int find_dominant_operator(int p,int q){
+    int i;
+    int qend;
+    int qleft,qright;
+    qleft=qright=0;
+    
+    /*
+    for(i=p;i<=q;i++)
+    {
+        if(tokens[i].type=='(')pleft++;
+        else if(tokens[i].type==')')pright++;
+        
+        if(pleft==pright){
+             pend=(pleft==0)?i:i+1;
+        }
+    }
+    */
+    qend=q;
+    do{
+
+        for(i=qend;i>=p;i--){
+            if(tokens[i].type=='(')qleft++;
+            else if(tokens[i].type==')')qright++;
+        
+            if(qleft==qright){
+                qend=(qleft==0)?i:i-1;
+            }
+        }
+
+
+        for(i=qend;i>=p;i--){
+            if(tokens[i].type=='*'||tokens[i].type=='/')return i;
+            else if(tokens[i].type==')')break;
+        }
+    }while(i>p);
+
+    qend=q;
+    do{
+
+        for(i=qend;i>=p;i--){
+            if(tokens[i].type=='(')qleft++;
+            else if(tokens[i].type==')')qright++;
+        
+            if(qleft==qright){
+                qend=(qleft==0)?i:i-1;
+            }
+        }
+
+
+        for(i=qend;i>=p;i--){
+            if(tokens[i].type=='+'||tokens[i].type=='-')return i;
+            else if(tokens[i].type==')')break;
+        }
+    }while(i>p);
+    
+    return -1;
+}
+
+
+int eval(int p,int q){
+    if(p>q){
+        panic("tokens index error");
+    }
+    else if(p==q){
+        if(tokens[p].type=='n'){
+            int val=atoi(tokens[p].str);
+            return val;
+        }
+        else panic("this is not a number");
+    }
+    else if(check_parentheses(p,q)==true){
+        return eval(p+1,q-1);
+    }
+    else{
+        int index=find_dominant_operator(p,q);
+        if(index==-1)panic("fail to find the dominant operator");
+        char op=tokens[index].str[0];
+        int val1=eval(p,index-1);
+        int val2=eval(index+1,q);
+        switch(op){
+            case '+':return val1+val2;
+            case '-':return val1-val2;
+            case '*':return val1*val2;
+            case '/':return val1/val2;
+            default:assert(0);
+        }
+    }
+}
+
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
 		return 0;
 	}
+    
 
+    
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	//panic("please implement me");
+    
+    return eval(0,nr_token-1);
 }
 
