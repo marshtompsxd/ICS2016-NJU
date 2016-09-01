669597940b11327d7e3330cb58a3b264785dbdcb
tracer-ics2016 <tracer@njuics.org>
2016-09-01 08:50:23 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:39 UTC 2016 x86_64 GNU/Linux  08:50:23 up 19:56,  4 users,  load average: 0.15, 0.09, 0.08 f5939f209c90f983c28998318668ff7cc02c0c3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -238,7 +238,8 @@ uint32_t expr(char *e, bool *success) {
     printf("nr_token is %d\n",nr_token); 
     int i;
     for(i=0;i<nr_token;i++){
-        printf("token[%d].type==%c\n",i,tokens[i].type);
+        printf("tokens[%d].type==%c\n",i,tokens[i].type);
+        printf("tokens[%d].str==%s\n",i,tokens[i].str);
     }
     
 	/* TODO: Insert codes to evaluate the expression. */
