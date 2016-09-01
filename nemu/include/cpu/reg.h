5b4176435f9e00146369f043363e1038164b9e85
tracer-ics2016 <tracer@njuics.org>
2016-09-01 15:47:19 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:39 UTC 2016 x86_64 GNU/Linux  15:47:19 up 1 day,  2:52,  3 users,  load average: 0.32, 0.19, 0.14 4a3e4daa74b24b6396c4384eb8b10a9828f55d51

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -30,8 +30,7 @@ typedef union {
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-    struct
-    {
+    struct{
 	    uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
         swaddr_t eip;
     };
