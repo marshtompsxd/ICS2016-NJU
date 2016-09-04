6a71557232c7ab3c3df48c1e0095a2a7701a93dc
tracer-ics2016 <tracer@njuics.org>
2016-09-04 16:02:41 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  16:02:41 up  1:54,  1 user,  load average: 0.34, 0.36, 0.28 cef4cc7bce5d0326aba6419e2b1f82eac2d312d3

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -37,7 +37,19 @@ typedef struct {
 	};
 
     swaddr_t eip;
-    
+    union{
+    	struct {
+    		unsigned CF:1;
+    		unsigned PF:1;
+    		unsigned ZF:1;
+    		unsigned SF:1;
+    		unsigned IF:1;
+    		unsigned DF:1;
+    		unsigned OF:1;
+    	};
+
+    	uint32_t eflags;
+	}eflags;
 
 } CPU_state;
 
