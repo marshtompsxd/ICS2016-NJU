3ebe326b7761765b4e9fdb1b30feddd65012d7ce
tracer-ics2016 <tracer@njuics.org>
2016-09-05 21:30:16 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  21:30:16 up  3:12,  1 user,  load average: 0.08, 0.08, 0.08 81476788fce4c37ed722cc68ad486911326c6705

--- nemu/src/cpu/exec/data-mov/leave.c
+++ nemu/src/cpu/exec/data-mov/leave.c
@@ -4,7 +4,7 @@
 make_helper(leave){
 	cpu.esp=cpu.ebp;
 	if(ops_decoded.is_operand_size_16){
-		cpu.ebp=swaddr_read(cpu.esp,4);
+		cpu.ebp=(swaddr_read(cpu.esp,4)&0xffff)&(cpu.esp&0xffff0000);
 		cpu.esp+=2;
 	}
 	else{
