283d7a8a060e9397f169d9b465ddb57cf8a125e5
tracer-ics2016 <tracer@njuics.org>
2016-09-05 09:31:34 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  09:31:34 up  1:06,  1 user,  load average: 0.51, 0.58, 0.65 6a92a70a4788fcb72375bc0491c40e11aa1d8a7d

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -5,11 +5,11 @@
 static void do_execute(){
 	cpu.esp-=DATA_BYTE;
 	if(DATA_BYTE==2){
-		MEM_W(cpu.esp,cpu.eip&0xffff);
+		MEM_W(cpu.esp,(cpu.eip&0xffff)+DATA_BYTE+1);
 		cpu.eip=(cpu.eip+op_src->val)&0xffff;
 	}
 	else{
-		MEM_W(cpu.esp,cpu.eip);
+		MEM_W(cpu.esp,cpu.eip+DATA_BYTE+1);
 		cpu.eip+=op_src->val;
 	}
 	print_asm_template1();
