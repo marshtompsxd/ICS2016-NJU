691dff4f4350256ca61aec6491c2cb47c07dbc55
tracer-ics2016 <tracer@njuics.org>
2016-09-05 23:28:43 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  23:28:43 up 35 min,  1 user,  load average: 0.25, 0.16, 0.14 e49821653fefe4640478629a1de8cc1788a7b07

--- nemu/src/cpu/exec/data-mov/push.c
+++ nemu/src/cpu/exec/data-mov/push.c
@@ -1,8 +1,8 @@
 #include "cpu/exec/helper.h"
 
-// #define DATA_BYTE 1
-// #include "push-template.h"
-// #undef DATA_BYTE
+#define DATA_BYTE 1
+#include "push-template.h"
+#undef DATA_BYTE
 
 #define DATA_BYTE 2
 #include "push-template.h"
@@ -13,3 +13,6 @@
 #undef DATA_BYTE
 
 make_helper_v(push_r)
+make_helper_v(push_rm)
+make_helper_v(push_i)
+
