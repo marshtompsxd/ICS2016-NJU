07f0fc694beb46840c19274f6ebe9fbac943e7e2
tracer-ics2016 <tracer@njuics.org>
2016-09-06 08:17:40 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  08:17:40 up 46 min,  1 user,  load average: 0.15, 0.13, 0.10 21a57d8f403cc62b55287c206677125b4e53df9c

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -7,7 +7,28 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	//panic("please implement me");
+
+	cpu.eflags.CF=0;
+	cpu.eflags.OF=0;
+
+	int count=0;
+	int temp=result;
+	int i;
+	for(i=0;i<8;i++){
+		if(temp&0x1)count++;
+		temp=temp>>1;
+	}
+
+	if(count%2==0)cpu.eflags.PF=1;
+	else cpu.eflags.PF=0;
+
+	if(result==0)cpu.eflags.ZF=1;
+	else cpu.eflags.ZF=0;
+
+	if(MSB(result)==1)cpu.eflags.SF=1;
+	else cpu.eflags.SF=0;
+
 
 	print_asm_template2();
 }
