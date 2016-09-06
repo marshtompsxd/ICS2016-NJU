6718ead22a16fb19abe7de0f7c6a8b24af71e463
tracer-ics2016 <tracer@njuics.org>
2016-09-06 08:05:00 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  08:05:00 up 34 min,  1 user,  load average: 0.05, 0.07, 0.09 69a5f5aa569704273049023e726a1b1db8db8a7

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,60 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute() {
+	DATA_TYPE addend1=op_src->val;
+	DATA_TYPE addend2=op_dest->val;
+	DATA_TYPE result=addend1+addend2+cpu.eflags.CF;
+
+	if(result==0)
+		cpu.eflags.ZF=1;
+	else 
+		cpu.eflags.ZF=0;
+
+	if(addend1>result||addend2>result)
+		cpu.eflags.CF=1;
+	else
+		cpu.eflags.CF=0;
+
+	int i;
+	int temp=result;
+	int count=0;
+	for(i=0;i<8;i++){
+		if((temp&0x1)==0x1)count++;
+		temp=temp>>1;
+	}
+
+	if(count%2==0)
+		cpu.eflags.PF=1;
+	else
+		cpu.eflags.PF=0;
+
+	if(MSB(result)==1)
+		cpu.eflags.SF=1;
+	else
+		cpu.eflags.SF=0;
+
+	if(MSB(result)!=MSB(addend1)&&MSB(addend1)==MSB(addend2))
+		cpu.eflags.OF=1;
+	else
+		cpu.eflags.OF=0;
+
+	OPERAND_W(op_dest,result);
+	
+
+	print_asm_template2();
+}
+
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#if DATA_BYTE==2||DATA_BYTE==4
+make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
+
