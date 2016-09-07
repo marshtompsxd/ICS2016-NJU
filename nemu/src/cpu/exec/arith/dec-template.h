e4b8215bfb7669b4a800025410aec62a54921b52
tracer-ics2016 <tracer@njuics.org>
2016-09-07 17:17:13 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  17:17:13 up  1:16,  1 user,  load average: 0.41, 0.22, 0.16 50c639f140ef381a2e116276fbf403e4774ba840

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -7,7 +7,35 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	//panic("please implement me");
+
+	if(result==0)
+		cpu.eflags.ZF=1;
+	else 
+		cpu.eflags.ZF=0;
+
+	int i;
+	int temp=result;
+	int count=0;
+	for(i=0;i<8;i++){
+		if((temp&0x1)==0x1)count++;
+		temp=temp>>1;
+	}
+
+	if(count%2==0)
+		cpu.eflags.PF=1;
+	else
+		cpu.eflags.PF=0;
+
+	if(MSB(result)==1)
+		cpu.eflags.SF=1;
+	else
+		cpu.eflags.SF=0;
+
+	if(MSB(result)!=MSB(op_src->val)&&MSB(op_src->val)!=0)
+		cpu.eflags.OF=1;
+	else
+		cpu.eflags.OF=0;
 
 	print_asm_template1();
 }
