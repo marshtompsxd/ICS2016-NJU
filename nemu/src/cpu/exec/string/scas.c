95074e054ef05479ebf152ceb99351882d6fd2d9
tracer-ics2016 <tracer@njuics.org>
2016-09-08 10:33:44 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  10:33:44 up 21 min,  1 user,  load average: 0.38, 0.28, 0.21 dc50c1a867b88b5686c468d8ef21256f7b75630c

--- nemu/src/cpu/exec/string/scas.c
+++ nemu/src/cpu/exec/string/scas.c
@@ -84,7 +84,7 @@ make_helper(scas_b){
 	else 
 		cpu.edi -= 1;
 	print_asm("scasb");
-	return 1;
+	return 0;
 }
 
 make_helper(scas_w){
@@ -129,7 +129,7 @@ make_helper(scas_w){
 	else
 		cpu.edi-=2;
 	print_asm("scasw");
-	return 1;
+	return 0;
 }
 
 make_helper(scas_l){
@@ -174,7 +174,7 @@ make_helper(scas_l){
 	else
 		cpu.edi-=4;
 	print_asm("scasl");
-	return 1;
+	return 0;
 }
 
 make_helper_v(scas)
