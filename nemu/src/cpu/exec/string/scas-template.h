05b345e634952d198e64316dd2dd61ccb4a77983
tracer-ics2016 <tracer@njuics.org>
2016-09-08 08:47:59 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  08:47:59 up 30 min,  1 user,  load average: 0.29, 0.14, 0.10 88a4d5a5e5234ef5f335bc9d682f3e2785db02bf

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -5,51 +5,35 @@
 static void do_execute(){
 
 	if(cpu.eflags.DF==0)
-	{
-		cpu.esi+=DATA_BYTE;
 		cpu.edi+=DATA_BYTE;
-	}
 	else
-	{
-		cpu.esi-=DATA_BYTE;
 		cpu.edi-=DATA_BYTE;
-	}
+	
 
-	if(cpu.esi<cpu.edi)
+	if(cpu.eax<cpu.edi)
 		cpu.eflags.CF=1;
 	else
 		cpu.eflags.CF=0;
 
-	if((MSB(cpu.esi)^MSB(cpu.esi-cpu.edi))&&(MSB(cpu.edi)^MSB(cpu.esi)))
+	if((MSB(cpu.eax)^MSB(cpu.eax-cpu.edi))&&(MSB(cpu.edi)^MSB(cpu.eax)))
 		cpu.eflags.OF=1;
 	else
 		cpu.eflags.OF=0;
 	
-	cpu.eflags.SF=!!MSB(cpu.esi-cpu.edi);
+	cpu.eflags.SF=!!MSB(cpu.eax-cpu.edi);
 
-	if((cpu.esi-cpu.edi)==0)
+	if((cpu.eax-cpu.edi)==0)
 		cpu.eflags.ZF=1;
 	else
 		cpu.eflags.ZF=0;
 
 	int count=0;
-	if(((cpu.esi-cpu.edi)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>1)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>2)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>3)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>4)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>5)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>6)&0x1)==0x1)
-		count++;
-	if((((cpu.esi-cpu.edi)>>7)&0x1)==0x1)
-		count++;
-
+	int i;
+	DATA_TYPE temp=cpu.eax-cpu.edi;
+	for(i=0;i<8;i++){
+		if((temp&0x1)==0x1)count++;
+		temp=temp>>1;
+	}
 	if(count%2==0)
 		cpu.eflags.PF=1;
 	else
