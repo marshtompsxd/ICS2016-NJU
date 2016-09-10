332e4dd020fef701efd18e7f7902c7046fadfab8
tracer-ics2016 <tracer@njuics.org>
2016-09-10 20:53:10 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  20:53:10 up 50 min,  1 user,  load average: 0.15, 0.18, 0.10 804ee88e9bbf17ec2319b53d6acfc0e549b01ef8

--- nemu/src/cpu/exec/string/lods.c
+++ nemu/src/cpu/exec/string/lods.c
@@ -2,7 +2,8 @@
 
 
 make_helper(lods_b){
-	cpu.gpr[0]._8[0]=swaddr_read(cpu.esi,1);
+	//cpu.gpr[0]._8[0]=swaddr_read(cpu.esi,1);
+	cpu.gpr[0]._8[0]= (uint8_t)cpu.esi&0xff;
 	if (cpu.eflags.DF == 0)	
 		cpu.esi += 1;
 	else 
@@ -12,7 +13,8 @@ make_helper(lods_b){
 }
 
 make_helper(lods_w){
-	cpu.gpr[0]._16=swaddr_read(cpu.esi,2);
+	//cpu.gpr[0]._16=swaddr_read(cpu.esi,2);
+	cpu.gpr[0]._16= (uint16_t)cpu.esi&0xffff;
 	if(cpu.eflags.DF==0)
 		cpu.esi+=2;
 	else
@@ -22,7 +24,8 @@ make_helper(lods_w){
 }
 
 make_helper(lods_l){
-	cpu.eax=swaddr_read(cpu.esi,4);
+	//cpu.eax=swaddr_read(cpu.esi,4);
+	cpu.eax=cpu.esi;
 	if(cpu.eflags.DF==0)
 		cpu.esi+=4;
 	else
