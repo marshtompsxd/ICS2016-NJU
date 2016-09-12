7bf0b596f45c710a4977976fef925dfa9b8e584e
tracer-ics2016 <tracer@njuics.org>
2016-09-12 19:42:33 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  19:42:33 up 57 min,  1 user,  load average: 0.21, 0.23, 0.18 196a65e790e0cccf821cb6b211ba207b817c38c5

--- nemu/src/cpu/exec/control-transfer/jmp-template.h
+++ nemu/src/cpu/exec/control-transfer/jmp-template.h
@@ -2,18 +2,22 @@
 
 #define instr jmp
 
-static void do_execute(){
-	if(op_src->type==OP_TYPE_IMM){
-		cpu.eip+=op_src->val;
+static void do_execute() {
+	if(op_src->type==OP_TYPE_IMM)
+	{
+		cpu.eip=cpu.eip+op_src->val;
 		if(DATA_BYTE==2)
-			cpu.eip=((cpu.eip+DATA_BYTE+1)&0xffff)-DATA_BYTE-1;
+			cpu.eip=cpu.eip&0x0000ffff;
 	}
-	else if(op_src->type==OP_TYPE_REG||op_src->type==OP_TYPE_MEM){
+	else if(op_src->type==OP_TYPE_MEM||op_src->type==OP_TYPE_REG)
+	{ 
 		if(DATA_BYTE==2)
-			cpu.eip=(op_src->val&0xffff)-DATA_BYTE-1;
+			cpu.eip=op_src->val&0x0000ffff;
 		else
-			cpu.eip=op_src->val-DATA_BYTE-1;
+			cpu.eip=op_src->val-2;
+//			cpu.eip=op_src->val;
 	}
+
 	print_asm_template1();
 }
 
