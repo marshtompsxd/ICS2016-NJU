a653f6efac331a717a3c11cc40c961a7f0d6bf1d
tracer-ics2016 <tracer@njuics.org>
2016-09-13 15:41:20 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  15:41:20 up 44 min,  1 user,  load average: 0.12, 0.10, 0.09 61d4d227c4effc8e9ae294f4b469dddc9fab30a6

--- nemu/src/cpu/exec/data-mov/cwtltd.c
+++ nemu/src/cpu/exec/data-mov/cwtltd.c
@@ -1,19 +1,10 @@
 #include "cpu/exec/helper.h"
 
 make_helper(cwtltd){
-	if(ops_decoded.is_operand_size_16){
-		if(((cpu.eax>>15)&0x1)==0x1)
-			cpu.edx=cpu.edx|0xffff;
-		else
-			cpu.edx=cpu.edx&0xffff0000;
-	}
-	else{
-		if(((cpu.eax>>31)&0x1)==0x1)
-			cpu.edx=0xffffffff;
-		else
-			cpu.edx=0;
-		
-	}
+	if(((cpu.eax>>31)&0x1)==0x1)
+		cpu.edx=0xffffffff;
+	else
+		cpu.edx=0;
 	print_asm("cwtltd");
 	return 1;
 }
