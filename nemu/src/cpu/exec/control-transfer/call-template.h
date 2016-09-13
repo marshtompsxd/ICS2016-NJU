71811fe272a0fa60d882f779da6e70392881bdf3
tracer-ics2016 <tracer@njuics.org>
2016-09-13 14:59:33 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  14:59:33 up 2 min,  1 user,  load average: 0.33, 0.28, 0.11 62c687a1ab57c08dbe9c517dfdc4e1c1d947a4e

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -2,89 +2,42 @@
 
 #define instr call 
 
-// static void do_execute(){
-// 	if(op_src->type==OP_TYPE_IMM){
-// 		if(DATA_BYTE==2){
-// 			cpu.esp-=DATA_BYTE;
-// 			swaddr_write(cpu.esp,DATA_BYTE,(cpu.eip+DATA_BYTE+1)&0x0000ffff);	
-// 			cpu.eip=((cpu.eip+DATA_BYTE+1+op_src->val)&0x0000ffff)-DATA_BYTE-1;
-// 		}
-// 		else{
-
-// 			cpu.esp-=DATA_BYTE;
-// 			swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+DATA_BYTE+1);
-// 			//printf("now is %x\n", cpu.eip);
-// 			//printf("store%x\n",cpu.eip+DATA_BYTE+1 );
-// 			cpu.eip+=op_src->val;
-// 			//printf("call %x\n", cpu.eip);
-
-// 		}
-// 		//printf("call \n");
-
-// 	}
-// else if(op_src->type==OP_TYPE_REG||op_src->type==OP_TYPE_MEM){
-// 	if(DATA_BYTE==2){
-// 		cpu.esp-=DATA_BYTE;
-
-// 		swaddr_write(cpu.esp,DATA_BYTE,(cpu.eip+2)&0x0000ffff);
-
-// 		cpu.eip=((op_src->val)&0x0000ffff)-2;
-//  	}
-// 	else{ 
-// 		cpu.esp-=DATA_BYTE;
-// 		swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+2);
-// 		cpu.eip=op_src->val-DATA_BYTE-2;
-//  	}
-// }
-// 	print_asm_template1();
-// }
-
-
 static void do_execute(){
-if(op_src->type==OP_TYPE_IMM)
-{
-	if(DATA_BYTE==2)
-	{
-		cpu.esp-=DATA_BYTE;
-		swaddr_write(cpu.esp,DATA_BYTE,(cpu.eip&0xffff)+DATA_BYTE+1);	
-		cpu.eip=(cpu.eip+op_src->val)&0x0000ffff;
+	if(op_src->type==OP_TYPE_IMM){
+		if(DATA_BYTE==2){
+			cpu.esp-=DATA_BYTE;
+			swaddr_write(cpu.esp,DATA_BYTE,(cpu.eip+DATA_BYTE+1)&0x0000ffff);	
+			cpu.eip=((cpu.eip+DATA_BYTE+1+op_src->val)&0x0000ffff)-DATA_BYTE-1;
+		}
+		else{
+
+			cpu.esp-=DATA_BYTE;
+			swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+DATA_BYTE+1);
+			//printf("now is %x\n", cpu.eip);
+			//printf("store%x\n",cpu.eip+DATA_BYTE+1 );
+			cpu.eip+=op_src->val;
+			//printf("call %x\n", cpu.eip);
+
+		}
+		//printf("call \n");
+
 	}
-	else
-	{
-
-	cpu.esp-=DATA_BYTE;
-	
-
-	swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+DATA_BYTE+1);
-	cpu.eip+=op_src->val;
-
-	}
-}
-else if(op_src->type==OP_TYPE_REG||op_src->type==OP_TYPE_MEM)
-{
-	if(DATA_BYTE==2)
- 	{
+else if(op_src->type==OP_TYPE_REG||op_src->type==OP_TYPE_MEM){
+	if(DATA_BYTE==2){
 		cpu.esp-=DATA_BYTE;
 
-		swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+3);
+		swaddr_write(cpu.esp,DATA_BYTE,(cpu.eip+2)&0x0000ffff);
 
-		cpu.eip=(op_src->val)&0x0000ffff;
+		cpu.eip=((op_src->val)&0x0000ffff)-2;
  	}
-	else
-	{ 
+	else{ 
 		cpu.esp-=DATA_BYTE;
-		swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+5);
-
-		cpu.eip=op_src->val-2;
+		swaddr_write(cpu.esp,DATA_BYTE,cpu.eip+2);
+		cpu.eip=op_src->val-DATA_BYTE-2;
  	}
 }
-
 	print_asm_template1();
-
 }
-
-
-
 
 make_instr_helper(i)
 make_instr_helper(rm)
--- nemu/src/cpu/exec/control-transfer/jmp-template.h
+++ nemu/src/cpu/exec/control-transfer/jmp-template.h
@@ -2,41 +2,37 @@
 
 #define instr jmp
 
-// static void do_execute() {
-// 	if(op_src->type==OP_TYPE_IMM)
-// 	{
-// 		cpu.eip=cpu.eip+op_src->val;
+// static void do_execute(){
+// 	if(op_src->type==OP_TYPE_IMM){
+// 		cpu.eip+=op_src->val;
 // 		if(DATA_BYTE==2)
 // 			cpu.eip=((cpu.eip+DATA_BYTE+1)&0xffff)-DATA_BYTE-1;
 // 	}
-// 	else if(op_src->type==OP_TYPE_MEM||op_src->type==OP_TYPE_REG)
-// 	{ 
+// 	else if(op_src->type==OP_TYPE_REG||op_src->type==OP_TYPE_MEM){
 // 		if(DATA_BYTE==2)
-// 			cpu.eip=(op_src->val&0x0000ffff)-2;
-			
-		
+// 			cpu.eip=(op_src->val&0xffff)-DATA_BYTE-1;
 // 		else
-// 			cpu.eip=op_src->val-2;
-			
-
+// 			cpu.eip=op_src->val-DATA_BYTE-1;
 // 	}
-
 // 	print_asm_template1();
 // }
-
 static void do_execute() {
 	if(op_src->type==OP_TYPE_IMM)
 	{
 		cpu.eip=cpu.eip+op_src->val;
 		if(DATA_BYTE==2)
-			cpu.eip=cpu.eip&0x0000ffff;
+			cpu.eip=((cpu.eip+DATA_BYTE+1)&0xffff)-DATA_BYTE-1;
 	}
 	else if(op_src->type==OP_TYPE_MEM||op_src->type==OP_TYPE_REG)
 	{ 
 		if(DATA_BYTE==2)
-			cpu.eip=op_src->val&0x0000ffff;
+			cpu.eip=(op_src->val&0x0000ffff)-2;
+			
+		
 		else
 			cpu.eip=op_src->val-2;
+			
+
 	}
 
 	print_asm_template1();
