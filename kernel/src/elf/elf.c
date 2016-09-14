b5726a42ad9c4406340ffef771d0c1fedad8d5cb
tracer-ics2016 <tracer@njuics.org>
2016-09-14 19:44:54 +0800

    >  compile kernel 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  19:44:54 up 50 min,  1 user,  load average: 0.56, 0.55, 0.35 35b578dc508bd58b971c9108a81a34d2eacfc444

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -45,12 +45,13 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 ramdisk_read((uint8_t *)(ph->p_vaddr),ph->p_offset,ph->p_filesz);
+			 uint32_t seg_paddr = ph->p_vaddr;
+			 ramdisk_read((void *)seg_paddr,ph->p_offset,ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			 memset((void *)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			 memset((void *)seg_paddr+ph->p_filesz,0,ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
