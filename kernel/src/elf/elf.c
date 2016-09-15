1bf3f454554c51f5ff8a20e28dd3522f187f05c3
tracer-ics2016 <tracer@njuics.org>
2016-09-15 16:11:35 +0800

    >  compile kernel 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  16:11:35 up 11 min,  1 user,  load average: 0.53, 0.57, 0.30 50c83e7fac92deae7bd846f2d330001eaea880c3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -40,18 +40,19 @@ uint32_t loader() {
 	int i;
 	for(i=0; i<elf->e_phnum;i++ ) {
 		/* Scan the program header table, load each segment into memory */
+		ph=(void*)(buf+elf->e_phoff+i*elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 uint32_t seg_paddr = ph->p_vaddr;
-			 ramdisk_read((void *)seg_paddr,ph->p_offset,ph->p_filesz);
+			
+			 ramdisk_read((uint8_t *)(ph->p_vaddr),ph->p_offset,ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			 memset((void *)seg_paddr+ph->p_filesz,0,ph->p_memsz - ph->p_filesz);
+			memset((void *)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
