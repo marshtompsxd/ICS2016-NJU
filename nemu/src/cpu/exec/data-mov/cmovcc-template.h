831487c45eefff0f707c13f5f7dcc8839830d57f
tracer-ics2016 <tracer@njuics.org>
2016-09-16 10:06:57 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  10:06:57 up 17 min,  1 user,  load average: 0.31, 0.29, 0.27 be15691da26d4766a46d2fd66c246803e751d6f7

--- nemu/src/cpu/exec/data-mov/cmovcc-template.h
+++ nemu/src/cpu/exec/data-mov/cmovcc-template.h
@@ -4,72 +4,71 @@
 	static void do_execute(){\
 		if(condition)\
 		{\
-			DATA_TYPE temp=op_src->val;\
-			OPERAND_W(op_dest,temp);\
+			OPERAND_W(op_dest,op_src->val);\
 		}\
-		print_asm_template1();\
+		print_asm_template2();\
 	}
 
 #define instr cmova
 make_do_execute(cpu.eflags.CF==0&&cpu.eflags.ZF==0)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovae
 make_do_execute(cpu.eflags.CF==0)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)        
+make_instr_helper(rm2r)        
 #endif
 #undef instr
 
 #define instr cmovb
 make_do_execute(cpu.eflags.CF==1)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovbe
 make_do_execute(cpu.eflags.CF==1||cpu.eflags.ZF==1)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmove
 make_do_execute(cpu.eflags.ZF==1)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovg
 make_do_execute(cpu.eflags.ZF==0||cpu.eflags.SF==cpu.eflags.OF)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovge
 make_do_execute(cpu.eflags.SF==cpu.eflags.OF)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovl
 make_do_execute(cpu.eflags.SF!=cpu.eflags.OF)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovle
 make_do_execute(cpu.eflags.ZF==1||cpu.eflags.SF!=cpu.eflags.OF)
 #if DATA_BYTE==2||DATA_BYTE==4
- make_instr_helper(rm)
+ make_instr_helper(rm2r)
 #endif
 #undef instr
 
@@ -77,49 +76,49 @@ make_do_execute(cpu.eflags.ZF==1||cpu.eflags.SF!=cpu.eflags.OF)
 #define instr cmovne
 make_do_execute(cpu.eflags.ZF==0)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif 
 #undef instr
 
 #define instr cmovno
 make_do_execute(cpu.eflags.OF==0)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovnp
 make_do_execute(cpu.eflags.PF==0)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovns
 make_do_execute(cpu.eflags.SF==0)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovo
 make_do_execute(cpu.eflags.OF==1)
 #if DATA_BYTE==2||DATA_BYTE==4 
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovp
 make_do_execute(cpu.eflags.PF==1)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif
 #undef instr
 
 #define instr cmovs
 make_do_execute(cpu.eflags.SF==1)
 #if DATA_BYTE==2||DATA_BYTE==4
-make_instr_helper(rm)
+make_instr_helper(rm2r)
 #endif 
 #undef instr
 
--- nemu/src/cpu/exec/data-mov/cmovcc.h
+++ nemu/src/cpu/exec/data-mov/cmovcc.h
@@ -1,22 +1,22 @@
 #ifndef __CMOVCC_H__                                                           
 #define __CMOVCC_H__ 
 
-make_helper(cmova_rm_v);
-make_helper(cmovae_rm_v);
-make_helper(cmovb_rm_v);
-make_helper(cmovbe_rm_v);
-make_helper(cmove_rm_v);
-make_helper(cmovg_rm_v);
-make_helper(cmovge_rm_v);
-make_helper(cmovl_rm_v);
-make_helper(cmovle_rm_v);
-make_helper(cmovne_rm_v);
-make_helper(cmovno_rm_v);
-make_helper(cmovnp_rm_v);
-make_helper(cmovns_rm_v);
-make_helper(cmovo_rm_v);
-make_helper(cmovp_rm_v);
-make_helper(cmovs_rm_v);
+make_helper(cmova_rm2r_v);
+make_helper(cmovae_rm2r_v);
+make_helper(cmovb_rm2r_v);
+make_helper(cmovbe_rm2r_v);
+make_helper(cmove_rm2r_v);
+make_helper(cmovg_rm2r_v);
+make_helper(cmovge_rm2r_v);
+make_helper(cmovl_rm2r_v);
+make_helper(cmovle_rm2r_v);
+make_helper(cmovne_rm2r_v);
+make_helper(cmovno_rm2r_v);
+make_helper(cmovnp_rm2r_v);
+make_helper(cmovns_rm2r_v);
+make_helper(cmovo_rm2r_v);
+make_helper(cmovp_rm2r_v);
+make_helper(cmovs_rm2r_v);
 
 
 #endif
