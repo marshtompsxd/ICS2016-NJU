ee2c06f5fb1bc6f1ec0ed4aa49687c4593dceaf9
tracer-ics2016 <tracer@njuics.org>
2016-09-18 19:50:58 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  19:50:57 up  1:32,  1 user,  load average: 0.13, 0.35, 0.31 8cf2cc4b4115f32cf69ff24781314d71ce3b7aa

--- nemu/src/cpu/exec/control-transfer/jmp-template.h
+++ nemu/src/cpu/exec/control-transfer/jmp-template.h
@@ -2,38 +2,57 @@
 
 #define instr jmp
 
+// static void do_execute() {
+// 	if(op_src->type==OP_TYPE_IMM)
+// 	{
+// 		cpu.eip=cpu.eip+op_src->val;
+// 		if(DATA_BYTE==2)
+// 			cpu.eip=((cpu.eip+DATA_BYTE+1)&0xffff)-DATA_BYTE-1;
+// 	}
+// 	else if(op_src->type==OP_TYPE_MEM||op_src->type==OP_TYPE_REG)
+// 	{ 
+// 		if(DATA_BYTE==2)
+// 			cpu.eip=(op_src->val&0x0000ffff)-2;
+			
+		
+// 		else
+// 			cpu.eip=op_src->val-2;
+			
+
+// 	}
+
+// 	print_asm_template1();
+// }
+
+
 static void do_execute() {
-	uint32_t opeip=cpu.eip;
-	uint32_t op=instr_fetch(opeip,1);
-	while(op==0x66){
-		opeip++;
-		op=instr_fetch(opeip,1);
-	}
-	print_asm_template1();
-	if(op==0xeb||op==0xe9){
-		
-		
+		uint32_t feip=cpu.eip;
+		uint32_t op=instr_fetch(cpu.eip,1);
+		while( op==0x66 )
+		{
+			feip=feip+1;
+			op=instr_fetch(feip,1);
+		}
+if( op==0xeb || op==0xe9 ){
+		DATA_TYPE_S off_set=op_src->val;
+		cpu.eip=cpu.eip+off_set;
 #if DATA_BYTE==2
-		DATA_TYPE_S offs=op_src->val;
-		cpu.eip=(cpu.eip+offs)&0xffff;
-
-#elif DATA_BYTE==4
-		DATA_TYPE_S offs=op_src->val;
-		cpu.eip+=offs;
-
+		cpu.eip=cpu.eip&0xffff;
 #endif
-	}
-	else if(op==0xff){
-#if DATA_BYTE==2
-			cpu.eip=(op_src->val)&0xffff;
-
-#elif DATA_BYTE==4
-			cpu.eip=op_src->val;
-
-#endif
-	}
 }
+else if( op==0xff ){
+		DATA_TYPE dest=op_src->val;
+#if DATA_BYTE==2
+		cpu.eip=dest&0xffff;
+#else
+		cpu.eip=dest;
+#endif
+}
+else
+	panic("please implement jmp");
 
+	print_asm_template1();
+}
 
 make_instr_helper(i)
 
@@ -44,7 +63,6 @@ make_instr_helper(cfrm)
 #if DATA_BYTE==1 || DATA_BYTE==4
 make_instr_helper(si)
 #endif
-
 
 
 #include "cpu/exec/template-end.h"
