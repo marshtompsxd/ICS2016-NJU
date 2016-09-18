f893db8b6db03af9944b3fc1c3df09e3af826dfa
tracer-ics2016 <tracer@njuics.org>
2016-09-18 19:41:59 +0800

    >  compile NEMU 151220097 marshtomp Linux ubuntu 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:00:59 UTC 2016 i686 i686 i686 GNU/Linux  19:41:59 up  1:23,  1 user,  load average: 0.76, 0.40, 0.30 d2330b238d77bd25936274f79572fff45745fb6f

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -3,43 +3,42 @@
 #define instr call 
 
 static void do_execute(){
-	uint32_t opeip=cpu.eip;
-	uint32_t op=instr_fetch(opeip,1);
-	while(op==0x66){
-		opeip++;
-		op=instr_fetch(opeip,1);
+	uint32_t feip=cpu.eip;
+	uint32_t op=instr_fetch(feip,1);
+	while( op==0x66 )
+	{
+		feip++;
+		op=instr_fetch(feip,1);
 	}
-	if(op==0xe8){
-		DATA_TYPE_S offs=op_src->val;
-		cpu.esp-=DATA_BYTE;
-
+	if( op==0xe8 ){
+	DATA_TYPE_S off_set=op_src->val;
 #if DATA_BYTE==2
-		
-		swaddr_write(cpu.esp,2,(cpu.eip+DATA_BYTE+2)&0xffff);
-		cpu.eip=(cpu.eip+offs)&0xffff;
-
+	cpu.esp-=2;
+	swaddr_write(cpu.esp,2,(cpu.eip+DATA_BYTE+2)&0xffff);
+	cpu.eip=(cpu.eip+off_set)&0xffff;
 #elif DATA_BYTE==4
-
-		swaddr_write(cpu.esp,4,cpu.eip+DATA_BYTE+1);
-		cpu.eip+=offs;
+	cpu.esp-=4;
+	swaddr_write(cpu.esp,4,cpu.eip+DATA_BYTE+1);
+	cpu.eip+=off_set;
 #endif
-		}
-		else if(op==0xff){
-			cpu.esp-=DATA_BYTE;
-
+	}
+	else if( op==0xff ){
 #if DATA_BYTE==2
-			cpu.eip=(op_src->val)&0xffff;
-
+	cpu.esp=cpu.esp-2;
+	//MEM_W(cpu.esp,(cpu.eip)&0XFFFF);
+	cpu.eip=( op_src->val )&0xffff;
 #elif DATA_BYTE==4
-			cpu.eip=op_src->val;
-
+	cpu.esp-=4;
+	//MEM_W(cpu.esp,cpu.eip+DATA_BYTE+1);
+	cpu.eip=op_src->val;
 #endif
-			int len=concat(decode_rm_,SUFFIX)(opeip+1);
-			swaddr_write(cpu.esp,DATA_BYTE,opeip+len+1);
-
-		}
-		else
-			panic("call is not right");
+	int len=concat(decode_rm_,SUFFIX)(feip+1);
+	swaddr_write(cpu.esp,DATA_BYTE,feip+len+1);
+	//Log("%x\n",feip+len+1);
+	//do_int3();
+	}
+	else
+		panic("please implement call.");
 	print_asm_template1();
 }
 
