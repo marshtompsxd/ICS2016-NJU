99bf6aabc7c378cd211c19431a51bad8a22dda77
tracer-ics2016 <tracer@njuics.org>
2016-10-10 15:05:12 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  15:05:12 up 17:01,  1 user,  load average: 0.21, 0.30, 0.26 5418e97ac4b5ff5b12ce419f688970aa3a8558ad

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -6,10 +6,10 @@ static void do_execute(){
 	if(DATA_BYTE==2){
 		cpu.esp-=2;
 		swaddr_write(cpu.esp,2,op_src->val);
-	}	
+	}
 	else {
 		cpu.esp-=4;
-		swaddr_write(cpu.esp,4,(DATA_TYPE)op_src->val);
+		swaddr_write(cpu.esp,4,op_src->val);
 	}
 	print_asm_template1();
 }
@@ -17,9 +17,10 @@ static void do_execute(){
 
 
 make_instr_helper(i)
-#if DATA_BYTE==2||DATA_BYTE==4
 make_instr_helper(r)
 make_instr_helper(rm)
+#if DATA_BYTE==1||DATA_BYTE==4
+make_instr_helper(si)
 #endif
 
 
