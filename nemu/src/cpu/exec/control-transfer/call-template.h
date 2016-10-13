813f7c972c59879159408815291e9467a00c67b2
tracer-ics2016 <tracer@njuics.org>
2016-10-13 21:12:07 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  21:12:07 up 13:33,  1 user,  load average: 0.32, 0.71, 0.67 5bab594ad68b268d31f26dd6fb7404919e160285

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -11,22 +11,22 @@ static void do_execute(){
 		op=instr_fetch(opeip,1);
 	}
 	if( op==0xe8 ){
-		DATA_TYPE_S offs=op_src->val;
+		//DATA_TYPE_S offs=op_src->val;
 		if(DATA_BYTE==2){
 			cpu.esp-=2;
 			MEM_W(cpu.esp,(cpu.eip+DATA_BYTE+2)&0xffff);
-			cpu.eip=(cpu.eip+offs)&0xffff;
+			cpu.eip=(cpu.eip+(DATA_TYPE_S)op_src->val)&0xffff;
 		}
 		else if(DATA_BYTE==4){
 			cpu.esp-=4;
 			MEM_W(cpu.esp,cpu.eip+DATA_BYTE+1);
-			cpu.eip+=offs;
+			cpu.eip+=(DATA_TYPE_S)op_src->val;
 		}
 	}
 	else if( op==0xff ){
 		if(DATA_BYTE==2){
 			cpu.esp=cpu.esp-2;
-			cpu.eip=( op_src->val )&0xffff;
+			cpu.eip=(op_src->val)&0xffff;
 		}
 		else if(DATA_BYTE==4){
 			cpu.esp-=4;
