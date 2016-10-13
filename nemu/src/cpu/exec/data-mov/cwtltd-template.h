008cdb7d4f6f86df0505d61562f61ca704376fe6
tracer-ics2016 <tracer@njuics.org>
2016-10-13 21:24:38 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 GNU/Linux  21:24:38 up 13:45,  1 user,  load average: 0.12, 0.58, 0.74 a13b9f29b66bb2064e3e2f4aa70b7a33c6ff1ffb

--- nemu/src/cpu/exec/data-mov/cwtltd-template.h
+++ nemu/src/cpu/exec/data-mov/cwtltd-template.h
@@ -3,19 +3,20 @@
 
 make_helper( concat(cwtltd_,SUFFIX) ) {
 
-#if DATA_BYTE==2
-	if(((DATA_TYPE_S)REG(R_AX))<0)
-		REG(R_DX)=0xFFFF;
-	else
-		REG(R_DX)=0;
-	print_asm("cwtl");
-#elif DATA_BYTE==4
-	if(((DATA_TYPE_S)REG(R_EAX))<0)
-		REG(R_EDX)=0xFFFFFFFF;
-	else
-		REG(R_EDX)=0;
-	print_asm("cltd");
-#endif
+	if (DATA_BYTE==2){
+		if(((DATA_TYPE_S)reg_w(R_AX))<0)
+			reg_w(R_DX)=0xffff;
+		else
+			reg_w(R_DX)=0;
+		print_asm("cwtl");
+	}
+	else if (DATA_BYTE==4){
+		if(((DATA_TYPE_S)reg_l(R_EAX))<0)
+			reg_l(R_EDX)=0xffffffff;
+		else
+			reg_l(R_EDX)=0;
+		print_asm("cltd");
+	}
 
     return 1;
 }
