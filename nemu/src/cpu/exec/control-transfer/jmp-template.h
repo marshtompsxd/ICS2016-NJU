06d2643c702f51753b0db2615b46f97a020b0ef4
tracer-ics2016 <tracer@njuics.org>
2016-10-29 09:11:37 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  09:11:37 up 11:54,  2 users,  load average: 0.66, 0.59, 0.41 5f9bc9df19c820bf9d4e98c26942950272ce705

--- nemu/src/cpu/exec/control-transfer/jmp-template.h
+++ nemu/src/cpu/exec/control-transfer/jmp-template.h
@@ -5,26 +5,24 @@
 
 
 static void do_execute() {
-		uint32_t opeip=cpu.eip;
-		uint32_t op=instr_fetch(cpu.eip,1);
-		/*get the opcode after the prefix 66 to tell which jmp it is*/
-		while( op==0x66 )
-		{
-			opeip=opeip+1;
-			op=instr_fetch(opeip,1);
-		}
-		if( op==0xeb || op==0xe9 ){
 
-			cpu.eip=cpu.eip+(DATA_TYPE_S)op_src->val;
-			if(DATA_BYTE==2)
-				cpu.eip=cpu.eip&0xffff;
-		}
-		else if( op==0xff ){
+
+		if( op_src->type==OP_TYPE_REG||op_src->type==OP_TYPE_MEM ){
 
 			if(DATA_BYTE==2)
 				cpu.eip=(DATA_TYPE)(op_src->val)&0xffff;
 			else
 				cpu.eip=(DATA_TYPE)(op_src->val);
+		}
+		if( op_src->type==OP_TYPE_IMM ){
+
+			if(DATA_BYTE==2){
+				cpu.eip=cpu.eip+(DATA_TYPE_S)op_src->val;
+				cpu.eip=cpu.eip&0xffff;
+			}
+			else{
+				cpu.eip=cpu.eip+(DATA_TYPE_S)op_src->val;
+			}
 		}
 		else
 			panic("please implement jmp");
--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -1,19 +1,19 @@
 #include "cpu/exec/template-start.h"
 
-#define instr pop 
+#define instr pop
 
 static void do_execute(){
-	
+
 	OPERAND_W(op_src,MEM_R(cpu.esp));
 	if(DATA_BYTE==2)cpu.esp+=2;
 	else cpu.esp+=4;
-	
+
 	print_asm_template1();
 }
 
 
 make_instr_helper(r)
-
+make_instr_helper(rm)
 
 
 
