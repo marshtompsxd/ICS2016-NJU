41e5c93056b5505042c849c4e404db974ff0ca8e
tracer-ics2016 <tracer@njuics.org>
2016-10-31 21:47:01 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  21:47:01 up 1 day,  1:05,  1 user,  load average: 0.62, 0.71, 0.52 624e02f15adbda953bea5b1e210370747daf8a8e

--- nemu/src/cpu/exec/control-transfer/ret-template.h
+++ nemu/src/cpu/exec/control-transfer/ret-template.h
@@ -6,40 +6,37 @@ static void do_execute () {
 	if (DATA_BYTE==2){
 		cpu.eip=cpu.eip&0xffff0000;
 		cpu.eip+=0xffff&MEM_R(cpu.esp);
-		//cpu.esp+=2;
 		cpu.eip=cpu.eip&0xffff;
 	}
 	else if (DATA_BYTE==4){
 		cpu.eip=MEM_R(cpu.esp);
-		//cpu.esp+=4;
 	}
 	cpu.esp+=DATA_BYTE;
 	int offset=((int)op_src->val)<<16>>16;
-	//op_src->val=offset;
+
 	cpu.esp=cpu.esp+offset;
 
 	print_asm_template1();
 }
 
 
-make_instr_helper(cfsi)
-
-
 make_helper( concat(ret_,SUFFIX) ){
 	if (DATA_BYTE==2){
 		cpu.eip=cpu.eip&0xffff0000;
 		cpu.eip+=0xffff&MEM_R(cpu.esp);
-		//cpu.esp+=2;
 		cpu.eip=cpu.eip&0xffff;
 	}
 	else if(DATA_BYTE==4){
 		cpu.eip=MEM_R(cpu.esp);
-		//cpu.esp+=4;
 	}
 	cpu.esp+=DATA_BYTE;
 	print_asm("ret");
 	return 0;
 }
+
+
+make_instr_helper(cfsi)
+
 
 
 #include "cpu/exec/template-end.h"
