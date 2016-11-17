cf87ded479c986634b851bca73bbcfb95b76a342
tracer-ics2016 <tracer@njuics.org>
2016-11-17 15:16:33 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  15:16:33 up  4:25,  1 user,  load average: 0.02, 0.12, 0.18 1175c1ea621f3702b8d52fe34be0bab560df6850

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -5,3 +5,29 @@
 #include <stdio.h>
 
 uint32_t dram_read(hwaddr_t,size_t);
+
+void dram_write(hwaddr_t,size_t,uint32_t);
+
+uint32_t cachel1_read(uint32_t,uint32_t);
+
+void cachel1_write(uint32_t,uint32_t,uint32_t);
+
+uint32_t cachel2_read(uint32_t,uint32_t);
+
+void cachel2_write(uint32_t,uint32_t,uint32_t);
+
+int rand();
+
+cachel1 CL1;
+
+double cache_miss_time=0;
+double cahce_visit_time=0;
+
+typedef union{
+    struct{
+        uint32_t block_addr : CL1_BLOCK_WIDTH;
+        uint32_t set_num : CL1_SET_WIDTH;
+        uint32_t tag_bit : CL1_TAG_WIDTH;
+    };
+    uint32_t addr;
+}cachel1_addr;
