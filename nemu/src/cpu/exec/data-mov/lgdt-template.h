978d46ee2e4e51bd0c447599f5b9db7e0cc261ec
tracer-ics2016 <tracer@njuics.org>
2016-11-20 09:40:40 +0800

    >  compile kernel 151220097 sunxudong Linux bb924824c45e 4.4.0-45-generic #66-Ubuntu SMP Wed Oct 19 14:12:37 UTC 2016 x86_64 GNU/Linux  09:40:40 up 12:08,  1 user,  load average: 0.48, 0.40, 0.27 51fdb6939d269bd98cbad88da3236bd41a5d305

---
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -0,0 +1,15 @@
+#include "cpu/exec/template-start.h"
+
+#define instr lgdt
+
+static void do_execute(){
+    if(DATA_BYTE==2){
+        
+    }
+    else{
+
+    }
+}
+
+
+#include "cpu/exec/template-end.h"
