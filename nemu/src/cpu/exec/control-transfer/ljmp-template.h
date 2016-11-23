ee2ce7e44d6935aa45102e4b0eaaf1b245581fd8
tracer-ics2016 <tracer@njuics.org>
2016-11-23 22:16:40 +0800

    >  test 151220097 sunxudong Linux bb924824c45e 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  22:16:40 up 13 min,  1 user,  load average: 0.09, 0.65, 0.61 e6e7f04ea49d4a0c937af8b93bab9037560d86c7

--- nemu/src/cpu/exec/control-transfer/ljmp-template.h
+++ nemu/src/cpu/exec/control-transfer/ljmp-template.h
@@ -3,35 +3,42 @@
 
 #define instr ljmp
 
+SegDesc desc;
+SegDescBase descbase;
+SegDescLimit desclimit;
+
+void loadbase(){
+    descbase._15_0=desc.base_15_0;
+    descbase._23_16=desc.base_23_16;
+    descbase._31_24=desc.base_31_24;
+}
+
+void loadlimit(){
+    desclimit._15_0=desc.limit_15_0;
+    desclimit._19_16=desc.limit_19_16;
+}
+
+
 make_helper(concat(ljmp_,SUFFIX)){
     if(DATA_BYTE==2){
         panic("please implment ljmp");
     }
     else{
-        SegDesc desc;
-        SegDescBase descbase;
-        SegDescLimit desclimit;
-
         cpu.sreg[SR_CS].selector.SELECTOR=instr_fetch(eip+5,2);
         cpu.eip=instr_fetch(eip+1,4);
-
 
         uint32_t index=cpu.sreg[SR_CS].selector.INDEX;
         desc.content[0]=lnaddr_read(cpu.gdtr.base+index*8,4);
         desc.content[1]=lnaddr_read(cpu.gdtr.base+index*8+4,4);
 
-        descbase._15_0=desc.base_15_0;
-        descbase._23_16=desc.base_23_16;
-        descbase._31_24=desc.base_31_24;
-
-        desclimit._15_0=desc.limit_15_0;
-        desclimit._19_16=desc.limit_19_16;
+        loadbase();
+        loadlimit();
 
         if(desc.granularity==0)
             cpu.sreg[SR_CS].hidden_descriptor.limit=desclimit.limit;
         else
-            //panic("please consider granularity is 1\n");
             cpu.sreg[SR_CS].hidden_descriptor.limit=((desclimit.limit+1)*(1<<12))-1;
+
         cpu.sreg[SR_CS].hidden_descriptor.base=descbase.base;
 
         print_asm("ljmp");
