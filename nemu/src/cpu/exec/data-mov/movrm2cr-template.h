7a312ad1ecc385538a489f89edff6e22db71b46c
tracer-ics2016 <tracer@njuics.org>
2016-11-24 11:05:08 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  11:05:08 up 13:02,  1 user,  load average: 0.27, 0.33, 0.27 403612e5276331be4f135dd7486eb7b31363b0b

--- nemu/src/cpu/exec/data-mov/movrm2cr-template.h
+++ nemu/src/cpu/exec/data-mov/movrm2cr-template.h
@@ -4,6 +4,7 @@
 
 static void do_execute(){
     if(op_dest->reg==0)cpu.cr0.val=op_src->val;
+    else if(op_dest->reg==3)cpu.cr3.val=op_src->val;
     else panic("please implment movrm2cr\n");
 
     print_asm_template2();
