87313bedb685326586cfaa9c5fe45e8492453805
tracer-ics2016 <tracer@njuics.org>
2016-11-27 19:16:30 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  19:16:30 up  3:11,  1 user,  load average: 0.59, 0.43, 0.34 4ee0ed9d16888ec09b48146465b08a242f4932b1

--- nemu/src/cpu/exec/data-mov/movrm2cr.c
+++
@@ -1,5 +0,0 @@
-#include "cpu/exec/helper.h"
-
-#define DATA_BYTE 4
-#include "movrm2cr-template.h"
-#undef DATA_BYTE
--- nemu/src/cpu/exec/data-mov/movrm2cr.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MOVRM2CR_H__
-#define __MOVRM2CR_H__
-
-make_helper(movrm2cr_rm2r_l);
-
-#endif
