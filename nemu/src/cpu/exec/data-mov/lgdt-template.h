6affdcfaea288b4a18025fb3154ac5a222b351e5
tracer-ics2016 <tracer@njuics.org>
2016-11-27 16:36:55 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  16:36:55 up 32 min,  1 user,  load average: 0.00, 0.06, 0.18 b7e55491deb3c3ce11fd0d3fbc8f1f4717c5cec8

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -7,12 +7,13 @@ static void do_execute(){
         panic("please implement lgdt\n");
     }
     else{
+        printf("%x\n", op_src->addr);
         cpu.gdtr.limit=lnaddr_read(op_src->addr,2);
         cpu.gdtr.base=lnaddr_read(op_src->addr+2,4);
     }
     printf("gdtr base is 0x%x\n",cpu.gdtr.base );
     printf("gdtr limit is 0x%x\n",cpu.gdtr.limit );
-    print_asm_template1();
+    print_asm("lgdt");
 }
 
 make_instr_helper(rm)
