2d36195a86df4ce75896abb3d5580bd5f37e2862
tracer-ics2016 <tracer@njuics.org>
2016-12-10 10:58:57 +0800

    >  compile NEMU 151220097 sunxudong Linux bb924824c45e 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  10:58:57 up  2:32,  1 user,  load average: 0.14, 0.29, 0.25 8cd05f0ab419779c1dca803e70bc9b6ce22ec566

--- nemu/src/cpu/exec/IO/in-template.h
+++ nemu/src/cpu/exec/IO/in-template.h
@@ -3,10 +3,10 @@
 #define instr in
 
 make_helper( concat(in_imm_,SUFFIX) ) {
-	decode_i_b(eip+1);
+	uint32_t len=decode_i_b(eip+1);
 	REG(R_EAX) = pio_read( op_src->val , DATA_BYTE);
 	print_asm("in %%%s,%u",REG_NAME(R_EAX),op_src->val );
-	return 2;
+	return 1+len;
 }
 make_helper( concat(in_dx_,SUFFIX) ) {
 	REG(R_EAX) = pio_read( cpu.edx & 0xffff , DATA_BYTE);
--- nemu/src/cpu/exec/IO/out-template.h
+++ nemu/src/cpu/exec/IO/out-template.h
@@ -3,10 +3,10 @@
 #define instr out
 
 make_helper( concat(out_imm_,SUFFIX) ) {
-	decode_i_b(eip+1);
+	uint32_t len=decode_i_b(eip+1);
 	pio_write( op_src->val, DATA_BYTE, REG(R_EAX) );
     print_asm("out 	%u,%%%s",op_src->val,REG_NAME(R_EAX) );
-	return 2;
+	return 1+len;
 }
 
 make_helper( concat(out_dx_,SUFFIX) ) {
