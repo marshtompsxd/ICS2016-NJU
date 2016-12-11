2ebfbb05197c408625f71130e3123bf10b31e4b2
tracer-ics2016 <tracer@njuics.org>
2016-12-11 10:21:56 +0800

    >  compile kernel 151220097 sunxudong Linux bb924824c45e 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 GNU/Linux  10:21:56 up 1 day,  1:55,  1 user,  load average: 0.06, 0.33, 0.58 f70f7a4e572b711922604d86afbf1f98bc68e

--- nemu/src/cpu/exec/control-transfer/call-template.h
+++ nemu/src/cpu/exec/control-transfer/call-template.h
@@ -10,24 +10,22 @@ static void do_execute(){
 	}
 	if( op_src->type==OP_TYPE_IMM ){
 		DATA_TYPE_S offs=op_src->val;
+		cpu.esp-=DATA_BYTE;
 		if(DATA_BYTE==2){
-			cpu.esp-=2;
 			MEM_W(cpu.esp,(cpu.eip+DATA_BYTE+2)&0xffff,SR_SS);
 			cpu.eip=(cpu.eip+offs)&0xffff;
 		}
 		else if(DATA_BYTE==4){
-			cpu.esp-=4;
 			MEM_W(cpu.esp,cpu.eip+DATA_BYTE+1,SR_SS);
 			cpu.eip+=offs;
 		}
 	}
 	else if( op_src->type==OP_TYPE_REG || op_src->type==OP_TYPE_MEM ){
+		cpu.esp-=DATA_BYTE;
 		if(DATA_BYTE==2){
-			cpu.esp=cpu.esp-2;
 			cpu.eip=( op_src->val )&0xffff;
 		}
 		else if(DATA_BYTE==4){
-			cpu.esp-=4;
 			cpu.eip=op_src->val;
 		}
 
